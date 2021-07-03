31427943eaf4dcd74c269361c7567ff0a05670ac
MANISHA NEGI <49955919+manishanegii@users.noreply.github.com>
2021-07-03 10:38:50 +0530

    commit

--- DAY3/4.cpp
+++ DAY3/4.cpp
@@ -5,21 +5,18 @@ vector<int> decode(vector<int>& encode, int num) {
         op.push_back(num);
         int ans;
         int s = encode.size();
-        for(int i=0;i<s;i++)
-        {
+        for(int i=0;i<s;i++){
             ans=op[i]^encode[i];
             op.push_back(ans);
         }
         return op;
     }
-int main()
-{
+int main(){
  vector<int> encode;
  int n;
  cin>>n;
  int val;
- for(int i=0;i<n;i++)
-    {
+ for(int i=0;i<n;i++){
         cin>>val;
         encode.push_back(val);
     }
